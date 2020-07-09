bdd571a712b74bf449cac70f1c2f73689d42d039
henok Gebremedhn <henoklwi@gmail.com>
2020-07-09 15:33:20 -0700

    complete store page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import {Provider} from 'react-redux';
-import Store from './Store';
+import store from './Store';
+import Products from './components/Products';
+import Category from './components/Categories';
 
 function App() {
   return (
     <div className="App">
-      <Provider Store={Store}>
+      <Provider store={store}>
 
   <Header/>
+  <Category/>
+  <Products/>
       </Provider>
     </div>
   );
--- src/Store.js
+++ src/Store.js
@@ -3,10 +3,11 @@ import {createStore} from 'redux';
 
 const initState = {
   cart: 0,
-  category : [{name:'Electronics' , displayName: 'Electronics '},{name:'Foods' , displayName: 'Foods'}],
+  category : [{name:'Electronics' , displayName: 'Electronics '},{name:'Food' , displayName: 'Foods'}],
 
   products : [{name:'Apple',description:'One apple a day',stoke:'30',price:'5',category:'Food'},{name:'Pizza',description:'The definition of happiness',stoke:'50',price:'10',category:'Food'},{name:'Kitfo',description:'Ethiopian beef ',stoke:'30',price:'15',category:'Food'} ,{name:'MacAir',description:'Easy to use',stoke:'3',price:'1000',category:'Electronics'},{name:'Tv',description:'Connect with world',stoke:'66',price:'700',category:'Electronics'}],
-  currentCategory:'Electronics',
+
+  currentCategory:'Food',
 };
 
 const reducer = (state = initState , action) =>{
